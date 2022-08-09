89075c102f6c14d37f5dd3a319f3fa24650ec951
rashidali001 <rashidestonian@gmail.com>
2022-08-09 16:58:28 -0700

    printf

---
+++ print_prg.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_prg - writes the character c to stdout
+ * @a: input char
+ * @buf: buffer pointer
+ * @i: index for buffer pointer
+ * Return: on success 1
+ */
+int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i)
+{
+	handl_buf(buf, '%', i);
+	return (1);
+}
